5e2ad62d756ea180b78674116b56df53395ee67e
Maziorow <410372@student.agh.edu.pl>
2025-11-26 15:40:21 +0000

    Bugfixy w skrypcie startujacym

--- .devcontainer/launch.sh
+++ .devcontainer/launch.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
 set -e
 
-if [ -f install/setup.bash ]; then source install/setup.bash; fi
-source /home/developer/.bashrc
 ./setup.sh
 ./build.sh
+
+source install/setup.bash 
+source /home/developer/.bashrc
+
+COMPUTER_NAME=$(hostname)
+
+echo "----------------------------------------"
+echo "Configuring Network for: $COMPUTER_NAME"
+
+case "$COMPUTER_NAME" in
+    "lsriw-wolowiec")
+        export ROS_DOMAIN_ID=67
+        echo "Match found! ID set to 67"
+        ;;
+    "lsriw-giewont")
+        export ROS_DOMAIN_ID=68
+        echo "Match found! ID set to 68"
+        ;;
+    "lsriw-koscielec")
+        export ROS_DOMAIN_ID=69
+        echo "Match found! ID set to 69"
+        ;;
+    "lsriw-krzesanica")
+        export ROS_DOMAIN_ID=70
+        echo "Match found! ID set to 70"
+        ;;
+    *)
+        export ROS_DOMAIN_ID=42
+        echo "Unknown computer name. Defaulting ROS_DOMAIN_ID to 42."
+        ;;
+esac
+echo "Final ROS_DOMAIN_ID: $ROS_DOMAIN_ID"
+echo "----------------------------------------"
+
 ros2 launch simple_example example.launch.py
