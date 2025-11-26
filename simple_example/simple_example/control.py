a7b6c3bb9ce3cd2214b2eb6f8d22fba598107b72
romannowak9 <roman.nowak9@interia.pl>
2025-11-26 15:36:27 +0000

    velocity from IMU - not working

--- simple_example/simple_example/control.py
+++ simple_example/simple_example/control.py
@@ -5,6 +5,7 @@ import math
 from geometry_msgs.msg import Point
 from std_msgs.msg import Float32
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Imu
 
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy
 
@@ -27,11 +28,49 @@ class VelocityPublisher(Node):
 
         self.publisher_ = self.create_publisher(Twist, '/model/vehicle_blue/cmd_vel', qos_profile=cmd_qos_profile)
         self.point_sub = self.create_subscription(Point, "/img_set_point", self.point_callback, qos_profile=point_qos_profile)
+        self.imu_sub = self.create_subscription(Imu, "/imu", self.imu_callback, qos_profile=point_qos_profile)
 
         # timer_period = 0.5  # seconds
         # self.timer = self.create_timer(timer_period, self.timer_callback)
         self.const_velocity = (2.0, 0.0, 0.0)
         self.vehicle_length = 1.0
+
+        self.curr_vel = 0.0
+
+        self.last_imu_time = None
+        self.curr_vel = 0.0
+        self.alpha = 0.5      # filtr dolnoprzepustowy
+        self.filtered_acc = 0.0
+
+    def imu_callback(self, imu_msg: Imu):
+        current_time = imu_msg.header.stamp.sec + imu_msg.header.stamp.nanosec * 1e-9  # s
+
+        if self.last_imu_time is None:
+            self.last_imu_time = current_time
+            return
+
+        dt = current_time - self.last_imu_time
+        self.last_imu_time = current_time
+
+        # if dt <= 0.0 or dt > 1.0:   # zabezpieczenie przed błędnym dt
+        #     return
+
+        acc_x = -imu_msg.linear_acceleration.x
+
+        # Filtr dolnoprepustowy IIR
+        self.filtered_acc = self.alpha * self.filtered_acc + (1 - self.alpha) * acc_x
+
+        self.curr_vel += self.filtered_acc * dt
+
+        # Ograniczenie prędkości
+        # self.curr_vel = max(min(self.curr_vel, 10.0), -2.0)
+
+        self.get_logger().info(f"IMU dt: {dt} s")
+        self.get_logger().info(f"Vel from IMU: {self.curr_vel:.3f} m/s")
+        self.get_logger().info(f"acceleration: {acc_x} m/s^2")
+        
+
+        
 
     def point_callback(self, point: Point):
         msg = Twist()
@@ -49,7 +88,7 @@ class VelocityPublisher(Node):
         calc_angle = pure_pursuit(
             vehicle_length=self.vehicle_length,
             error_angle=err_angle,
-            forward_velocity=self.const_velocity[0]
+            forward_velocity=self.const_velocity[0]  # Żeby brało prędkość z IMU podmienić na self.curr_vel
         )
         msg.angular.x = 0.0
         msg.angular.y = 0.0
